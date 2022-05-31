30a749a29404bfc712c4e6e640bf070fdea77ce2
Fabricio <medinajose3195@gmail.com>
2022-05-31 19:24:09 +0200

    Modifica la estructura del search y se agrega un estado

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,29 +1,43 @@
-import { useContext } from "react";
-import { Form } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Form, Button, InputGroup } from "react-bootstrap";
 import { CocktailContext } from "../../context/CocktailContext";
+import SearchIcon from "../../assets/icons/search-icon.svg";
 
 const Search = () => {
+  const [form, setForm] = useState("");
   const { getData } = useContext(CocktailContext);
 
   const handleChange = (e) => {
-    let character = e.target.value;
-    getData(character);
+    setForm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (form.trim() === "") return;
+
+    getData(form);
   };
 
   return (
     <>
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label className="text-white">
-            Ingrese su cocktail favorito
-          </Form.Label>
+      <Form onSubmit={handleSubmit}>
+        <InputGroup className="mb-3">
+          <Button type="submit" variant="light" className="shadow-none">
+            <img
+              style={{ width: "1.5rem" }}
+              src={SearchIcon}
+              alt="search icon"
+            />
+          </Button>
           <Form.Control
             type="text"
-            placeholder="Ej. vodka"
+            placeholder="Encuentra tus cocktails favoritos"
             name="cocktail"
             onChange={handleChange}
+            className="py-2 shadow-none border-0"
           />
-        </Form.Group>
+        </InputGroup>
       </Form>
     </>
   );
